866e7b8902c350d1de696e89969932087fb1e9bb
EkaSaputra15 <ekasaputra0262@gmail.com>
2024-10-12 14:54:19 +0700

    Update index.php
    index.php

--- Praktikum2/index.php
+++ Praktikum2/index.php
@@ -1,49 +1,53 @@
 <?php
-include "Orang.php";
+include "orang.php";
 include "visibility.php";
+include "nilai.php";
 ?>
 
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Praktikum2</title>
+    <title>Praktikum 2</title>
 </head>
 <body>
-    <h1>Praktikum2</h1>
-<div>
-    <?php
-        $eka = new Orang();
-        $eka->nama = "Eka Saputra";
+    <h1>Praktikum 2</h1>
+    <div>
+        <?php
+            $eka = new orang();
+            $eka->nama = " Eka Saputra";
 
-        $eka->ucapsalam();
+            $eka->ucapsalam();
 
-    echo "<br>";
-        $dendi = new Orang();
-        $dendi->nama = "M. dendi saputra";
-        $dendi->ucapSalam();
+            echo "<br>";
 
-        echo "<br>";
+            $Indo = new orang();
+            $Indo->nama = " Indo Masse";
+            $Indo->ucapsalam();
 
-        $visibility = new visibility();
-        $visibility->tampilkanProperty();
+            echo "<br>";
 
-        echo " Ini akses di luar kelas <br>";
-        echo "public : " . $visibility->public . '<br>';
-        echo "protected : " . $visibility->protected . '<br>';
-        echo "private : " . $visibility->private . '<br>';
+            $visibility = new visibility();
+            $visibility->tampilkanproperty();
 
-        echo "<br> <br>";
+            echo "ini akses di luar kelas";
+            echo " public : " . $visibility->public . '<br>';
+            //echo "protected : " . $visibility->protected . '<br>';
+            //echo "private : " . $visibility->private . '<br>';
 
-        echo "Nilai MK Pemrograman web:";
-        $nilai = new Nilai();
-        $nilai->setTugas(79);
-        $nilai->setUts(90);
-        $nilai->setUas(89);
+            echo "<br> <br>";
 
-        echo "Nilai"
+            echo "Nilai MK Pemograman Web: <br>";
+            $nilai = new Nilai();
+            $nilai->setTugas(79);
+            $nilai->setUts(90);
+            $nilai->setUas(89);
 
-    ?>
-</div>
+            echo "Nilai UTS : " . $nilai->getUts() . "<br>";
+            echo "Nilai UAS : " . $nilai->getUas() . "<br>";
+            echo "Nilai Tugas : " . $nilai->getTugas() . "<br>";
+            echo "Total Nilai : " . $nilai->hitungTotal() . "<br>";
+        ?>
+    </div>
 </body>
 </html>

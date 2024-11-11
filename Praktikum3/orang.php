6ab14f1f05b2a7eebdd3a120e2d9204d797b8068
EkaSaputra15 <ekasaputra0262@gmail.com>
2024-11-11 08:49:53 +0700

    Delete Praktikum3 directory

--- Praktikum3/orang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Class Orang{
-    protected $nama;
-
-    public function ucapSalam(){
-        echo "Hallo perkenalkan nama saya " . $this->nama . "<br>";
-    }
-}
--- Praktikum3/orangbiasa.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once "Orang.php";
-
-class OrangBiasa extends Orang {
-    
-}

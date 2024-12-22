2bf5b2c1d0d44d2efd1fb000c310b8d86412a627
إيكا سابوترا <ekasaputra0262@gmail.com>
2024-12-22 22:15:16 +0700

    Tugas 5

---
+++ Praktikum5/controller/bukuController.php
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+<?php 
+
+require_once 'model/listBuku.php';
+
+class bukuController{
+  public function jalankan(){
+    // menggunakan model
+    $daftar_buku_model=new listBuku();
+    $daftar_buku= $daftar_buku_model->getData();
+
+    // mengirim dan menampilkan data ke view
+    include_once 'view/bukuView.php';
+  }
+=======
+<?php 
+
+require_once 'model/listBuku.php';
+
+class bukuController{
+  public function jalankan(){
+    // menggunakan model
+    $daftar_buku_model=new listBuku();
+    $daftar_buku= $daftar_buku_model->getData();
+
+    // mengirim dan menampilkan data ke view
+    include_once 'view/bukuView.php';
+  }
+>>>>>>> 753e988390e1b395992f377d210eaa17c493d6e6
+}
---
+++ Praktikum5/index.php
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+<?php 
+
+require_once "controller/bukuController.php";
+
+// menjalankan controller
+$app = new bukuController();
+$app->jalankan();
+=======
+<?php 
+
+require_once "controller/bukuController.php";
+
+// menjalankan controller
+$app = new bukuController();
+$app->jalankan();
+>>>>>>> 753e988390e1b395992f377d210eaa17c493d6e6
+?>
